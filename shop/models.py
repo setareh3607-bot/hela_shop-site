fa48ef49e807d32125e6f2b87ccac57762b6cffe
TaherehRoudbari <setareh3607@gmail.com>
2026-09-05 18:52:39 -0400

    add models

--- shop/models.py
+++ shop/models.py
@@ -1,11 +1,12 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=100, unique=True, verbose_name='عنوان دسته یندی')
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True, verbose_name='شناسه آدرس')
-    parent = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True, related_name='children', verbose_name='دسته بندی والد') 
+    parent = models.ForeignKey("self", on_delete=models.SET_NULL, blank=True, null=True, related_name='children', verbose_name='دسته بندی والد') 
     
     class Meta:
         verbose_name ='دسته بندی'
